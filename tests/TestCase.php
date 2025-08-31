a948dab93fe90308ba03c4d258c7eb60f08d1161
Francesco <inerba@gmail.com>
2025-08-31 16:43:12 +0200

    Remove unused service providers from TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,57 +2,21 @@
 
 namespace Inerba\DbConfig\Tests;
 
-use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
-use BladeUI\Icons\BladeIconsServiceProvider;
-use Filament\Actions\ActionsServiceProvider;
-use Filament\FilamentServiceProvider;
-use Filament\Forms\FormsServiceProvider;
-use Filament\Infolists\InfolistsServiceProvider;
-use Filament\Notifications\NotificationsServiceProvider;
-use Filament\SpatieLaravelSettingsPluginServiceProvider;
-use Filament\SpatieLaravelTranslatablePluginServiceProvider;
-use Filament\Support\SupportServiceProvider;
-use Filament\Tables\TablesServiceProvider;
-use Filament\Widgets\WidgetsServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Inerba\DbConfig\DbConfigServiceProvider;
-use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
 
 class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
         parent::setUp();
-
-        // Factory::guessFactoryNamesUsing(
-        //     fn (string $modelName) => 'Inerba\\DbConfig\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
-        // );
-
-        // Eseguiamo la migrazione del pacchetto prima di ogni test.
-        // Questo assicura un database pulito per ogni scenario.
-        $migration = include __DIR__ . '/../database/migrations/create_db_config_table.php.stub';
-        $migration->up();
+        // $migration = include __DIR__ . '/../database/migrations/create_db_config_table.php.stub';
+        // $migration->up();
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            // ActionsServiceProvider::class,
-            // BladeCaptureDirectiveServiceProvider::class,
-            // BladeHeroiconsServiceProvider::class,
-            // BladeIconsServiceProvider::class,
-            // FilamentServiceProvider::class,
-            // FormsServiceProvider::class,
-            // InfolistsServiceProvider::class,
-            // LivewireServiceProvider::class,
-            // NotificationsServiceProvider::class,
-            // SpatieLaravelSettingsPluginServiceProvider::class,
-            // SpatieLaravelTranslatablePluginServiceProvider::class,
-            // SupportServiceProvider::class,
-            // TablesServiceProvider::class,
-            // WidgetsServiceProvider::class,
             DbConfigServiceProvider::class,
         ];
     }
